7acae5fc8f1e47206287941124c8f99b92f795ec
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2025-05-04 15:44:49 +0300

    Admin Ödeme Geçmişi

--- backend/MacFit/AdminForm.Designer.cs
+++ backend/MacFit/AdminForm.Designer.cs
@@ -51,10 +51,9 @@
             this.UyelikBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(68)))));
             this.UyelikBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.UyelikBtn.ForeColor = System.Drawing.Color.White;
-            this.UyelikBtn.Location = new System.Drawing.Point(16, 192);
-            this.UyelikBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.UyelikBtn.Location = new System.Drawing.Point(12, 156);
             this.UyelikBtn.Name = "UyelikBtn";
-            this.UyelikBtn.Size = new System.Drawing.Size(240, 55);
+            this.UyelikBtn.Size = new System.Drawing.Size(180, 45);
             this.UyelikBtn.TabIndex = 2;
             this.UyelikBtn.Text = "Antrenör Kaydet";
             this.UyelikBtn.Click += new System.EventHandler(this.UyelikBtn_Click);
@@ -70,10 +69,9 @@
             this.SeansBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(68)))));
             this.SeansBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.SeansBtn.ForeColor = System.Drawing.Color.White;
-            this.SeansBtn.Location = new System.Drawing.Point(16, 306);
-            this.SeansBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SeansBtn.Location = new System.Drawing.Point(12, 249);
             this.SeansBtn.Name = "SeansBtn";
-            this.SeansBtn.Size = new System.Drawing.Size(240, 55);
+            this.SeansBtn.Size = new System.Drawing.Size(180, 45);
             this.SeansBtn.TabIndex = 2;
             this.SeansBtn.Text = "Seans Oluştur";
             this.SeansBtn.Click += new System.EventHandler(this.SeansBtn_Click);
@@ -89,10 +87,9 @@
             this.YagOraniBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(68)))));
             this.YagOraniBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.YagOraniBtn.ForeColor = System.Drawing.Color.White;
-            this.YagOraniBtn.Location = new System.Drawing.Point(16, 421);
-            this.YagOraniBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.YagOraniBtn.Location = new System.Drawing.Point(12, 342);
             this.YagOraniBtn.Name = "YagOraniBtn";
-            this.YagOraniBtn.Size = new System.Drawing.Size(240, 55);
+            this.YagOraniBtn.Size = new System.Drawing.Size(180, 45);
             this.YagOraniBtn.TabIndex = 2;
             this.YagOraniBtn.Text = "Workout Plan Oluştur";
             this.YagOraniBtn.Click += new System.EventHandler(this.YagOraniBtn_Click);
@@ -108,12 +105,12 @@
             this.GOdemelerBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(68)))));
             this.GOdemelerBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.GOdemelerBtn.ForeColor = System.Drawing.Color.White;
-            this.GOdemelerBtn.Location = new System.Drawing.Point(16, 535);
-            this.GOdemelerBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GOdemelerBtn.Location = new System.Drawing.Point(12, 435);
             this.GOdemelerBtn.Name = "GOdemelerBtn";
-            this.GOdemelerBtn.Size = new System.Drawing.Size(240, 55);
+            this.GOdemelerBtn.Size = new System.Drawing.Size(180, 45);
             this.GOdemelerBtn.TabIndex = 3;
             this.GOdemelerBtn.Text = "Geçmiş Ödemeler";
+            this.GOdemelerBtn.Click += new System.EventHandler(this.GOdemelerBtn_Click);
             // 
             // panel2
             // 
@@ -126,9 +123,8 @@
             this.panel2.Controls.Add(this.SeansBtn);
             this.panel2.Controls.Add(this.UyelikBtn);
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(281, 858);
+            this.panel2.Size = new System.Drawing.Size(211, 697);
             this.panel2.TabIndex = 1;
             // 
             // guna2Button1
@@ -142,10 +138,9 @@
             this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(68)))));
             this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Location = new System.Drawing.Point(16, 757);
-            this.guna2Button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2Button1.Location = new System.Drawing.Point(12, 615);
             this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(240, 55);
+            this.guna2Button1.Size = new System.Drawing.Size(180, 45);
             this.guna2Button1.TabIndex = 4;
             this.guna2Button1.Text = "Şifre Değiştir";
             this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
@@ -154,10 +149,9 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.Image = global::MacFit.Properties.Resources.macfit_black;
-            this.pictureBox1.Location = new System.Drawing.Point(16, 27);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 22);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(227, 78);
+            this.pictureBox1.Size = new System.Drawing.Size(170, 63);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -173,23 +167,21 @@
             this.KaloriBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(68)))));
             this.KaloriBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.KaloriBtn.ForeColor = System.Drawing.Color.White;
-            this.KaloriBtn.Location = new System.Drawing.Point(16, 650);
-            this.KaloriBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.KaloriBtn.Location = new System.Drawing.Point(12, 528);
             this.KaloriBtn.Name = "KaloriBtn";
-            this.KaloriBtn.Size = new System.Drawing.Size(240, 55);
+            this.KaloriBtn.Size = new System.Drawing.Size(180, 45);
             this.KaloriBtn.TabIndex = 2;
             this.KaloriBtn.Text = "Üye Listele";
             this.KaloriBtn.Click += new System.EventHandler(this.KaloriBtn_Click);
             // 
             // AdminForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(1759, 858);
+            this.ClientSize = new System.Drawing.Size(1319, 697);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AdminForm";
             this.Text = "AdminForm";
             this.panel2.ResumeLayout(false);
